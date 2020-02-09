882c7c10f70330dbf4581e07fd423ca652deafb5
RavenSolo <ralina.vova@yandex.ru>
2020-02-09 19:06:37 +0500

    последняя правка
    последняя правка

--- step 5/homework/homework.cpp
+++ step 5/homework/homework.cpp
@@ -14,13 +14,18 @@ int main()
 
 
 
-	int arr[2][4]{ 1,2,3,4,5,6 };
-	int* pa = (int*)arr;
+	//int arr[2][4]{ 1,2,3,4,5,6 };
+	//int* pa = (int*)arr;
 //	for (int i=0; i < 6; i++)
 //		cout << *pa++;//это как pa[i];
 
 
-#define mas(x,y,p,s)*(p+x*s+y)
-	cout << (mas(0, 0, pa, 4) = 5);
-		
+//#define mas(x,y,p,s)*(p+x*s+y)
+//	cout << (mas(0, 0, pa, 4) = 5);
+	
+	int* a = new int[10];
+	int* a = new int;
+
+
+
 }
