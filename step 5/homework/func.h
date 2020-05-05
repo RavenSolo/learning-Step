b0533a98a6d585538280eea4c93973fd470cd7fa
RavenSolo <51849328+RavenSolo@users.noreply.github.com>
2020-05-05 23:57:33 +0500

    решение задач 5.0
    Функции. Перегрузка функций
    создание проекта, перенос функций в файл и создание файла заголовков

---
+++ step 5/homework/func.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef func_h
+#define func_h
+
+
+void init(int arrey[], int N)  //прототип функции 
+
+
+
+
+#endif //Header
---
+++ step 5/homework/function.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+void init(int arrey[],int N) 
+{
+	for (int i = 0; i < N; i++)
+	{
+		arrey[i] = rand;
+
+
+	}
+	
+}
